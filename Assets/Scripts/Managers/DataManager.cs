65cadaf50365c70ee9b75eb7696e19347177c7b9
kiyomin0615 <moeday1010@gmail.com>
2024-10-31 16:24:47 +0900

    store data in dictionary

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class DataManager
 {
+    public Dictionary<int, Stat> statDict = new Dictionary<int, Stat>();
+
     public void Init()
     {
         TextAsset textAsset = Manager.Resource.Load<TextAsset>("Data/StatData");
         StatData statData = JsonUtility.FromJson<StatData>(textAsset.text); // Deserialize
+
         foreach (Stat stat in statData.stats)
         {
-            Debug.Log($"LEVEL: {stat.level}, HP: {stat.level}, ATTACK: {stat.attack}");
+            statDict.Add(stat.level, stat);
         }
     }
 }
